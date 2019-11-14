8f1c1a41162a3713cf3935613cf1854e11555a43
chiem3p <chiem2p@gmail.com>
2019-11-14 20:10:22 +0700

    update

--- src/cPort.java
+++ src/cPort.java
@@ -1,4 +1,4 @@
-
+    
 import Client.Client;
 import java.awt.Font;
 import javax.swing.JOptionPane;
@@ -38,39 +38,36 @@ public class cPort extends javax.swing.JFrame {
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
 
-        title = new javax.swing.JLabel();
-        imgTitle = new javax.swing.JLabel();
         back = new javax.swing.JButton();
+        title = new javax.swing.JLabel();
         imgBack = new javax.swing.JLabel();
+        inP = new javax.swing.JTextField();
         txtINP = new javax.swing.JLabel();
         jLabel3 = new javax.swing.JLabel();
         jLabel5 = new javax.swing.JLabel();
         jLabel4 = new javax.swing.JLabel();
-        inP = new javax.swing.JTextField();
         jLabel2 = new javax.swing.JLabel();
+        imgTitle = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
         inP1 = new javax.swing.JTextField();
         inP2 = new javax.swing.JTextField();
+        boderInp1 = new javax.swing.JLabel();
+        jLabel9 = new javax.swing.JLabel();
+        jLabel10 = new javax.swing.JLabel();
+        jLabel11 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTable1 = new javax.swing.JTable();
-        jLabel6 = new javax.swing.JLabel();
+        jLabel7 = new javax.swing.JLabel();
+        jPanel1 = new javax.swing.JPanel();
         BG = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Quét port");
+        setMinimumSize(new java.awt.Dimension(1920, 1080));
         setPreferredSize(new java.awt.Dimension(1920, 1080));
         setResizable(false);
         getContentPane().setLayout(null);
-
-        title.setFont(new java.awt.Font("Tahoma", 1, 45)); // NOI18N
-        title.setForeground(new java.awt.Color(255, 255, 255));
-        title.setText("Quét Port");
-        getContentPane().add(title);
-        title.setBounds(840, 40, 460, 60);
-
-        imgTitle.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/1b.png"))); // NOI18N
-        getContentPane().add(imgTitle);
-        imgTitle.setBounds(360, -10, 1090, 210);
 
         back.setFont(new java.awt.Font("Tahoma", 1, 24)); // NOI18N
         back.setText("Back");
@@ -81,36 +78,17 @@ public class cPort extends javax.swing.JFrame {
             }
         });
         getContentPane().add(back);
-        back.setBounds(90, 980, 160, 60);
+        back.setBounds(90, 960, 160, 60);
+
+        title.setFont(new java.awt.Font("Tahoma", 1, 48)); // NOI18N
+        title.setForeground(new java.awt.Color(255, 255, 255));
+        title.setText("Quét Port");
+        getContentPane().add(title);
+        title.setBounds(850, 40, 460, 60);
 
         imgBack.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/back.png"))); // NOI18N
         getContentPane().add(imgBack);
-        imgBack.setBounds(70, 980, 230, 60);
-
-        txtINP.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
-        txtINP.setForeground(new java.awt.Color(255, 255, 255));
-        txtINP.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        txtINP.setText("Nhập IP: ");
-        txtINP.setToolTipText("");
-        getContentPane().add(txtINP);
-        txtINP.setBounds(410, 190, 420, 80);
-
-        jLabel3.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
-        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel3.setText("Khoảng: ");
-        getContentPane().add(jLabel3);
-        jLabel3.setBounds(400, 290, 430, 60);
-
-        jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/txtInpCity.png"))); // NOI18N
-        getContentPane().add(jLabel5);
-        jLabel5.setBounds(400, 280, 440, 80);
-
-        jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/txtInpCity.png"))); // NOI18N
-        getContentPane().add(jLabel4);
-        jLabel4.setBounds(400, 190, 440, 80);
+        imgBack.setBounds(70, 960, 230, 60);
 
         inP.setFont(new java.awt.Font("Tahoma", 0, 36)); // NOI18N
         inP.setHorizontalAlignment(javax.swing.JTextField.CENTER);
@@ -126,17 +104,46 @@ public class cPort extends javax.swing.JFrame {
             }
         });
         getContentPane().add(inP);
-        inP.setBounds(830, 197, 760, 70);
+        inP.setBounds(810, 180, 750, 60);
+
+        txtINP.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
+        txtINP.setForeground(new java.awt.Color(255, 255, 255));
+        txtINP.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        txtINP.setText("Nhập IP: ");
+        txtINP.setToolTipText("");
+        getContentPane().add(txtINP);
+        txtINP.setBounds(380, 170, 400, 80);
+
+        jLabel3.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
+        jLabel3.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel3.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel3.setText("Khoảng: ");
+        getContentPane().add(jLabel3);
+        jLabel3.setBounds(370, 270, 420, 60);
+
+        jLabel5.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel5.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/txtInpCity.png"))); // NOI18N
+        getContentPane().add(jLabel5);
+        jLabel5.setBounds(370, 260, 440, 80);
+
+        jLabel4.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        jLabel4.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/txtInpCity.png"))); // NOI18N
+        getContentPane().add(jLabel4);
+        jLabel4.setBounds(370, 170, 440, 80);
 
         jLabel2.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/border.png"))); // NOI18N
         getContentPane().add(jLabel2);
-        jLabel2.setBounds(400, 190, 1210, 80);
+        jLabel2.setBounds(370, 170, 1210, 80);
+
+        imgTitle.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/1b.png"))); // NOI18N
+        getContentPane().add(imgTitle);
+        imgTitle.setBounds(360, 0, 1090, 180);
 
         jLabel1.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
         jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         jLabel1.setText("-->");
         getContentPane().add(jLabel1);
-        jLabel1.setBounds(1160, 280, 120, 80);
+        jLabel1.setBounds(1130, 260, 120, 80);
 
         inP1.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
         inP1.setHorizontalAlignment(javax.swing.JTextField.CENTER);
@@ -152,7 +159,7 @@ public class cPort extends javax.swing.JFrame {
             }
         });
         getContentPane().add(inP1);
-        inP1.setBounds(840, 290, 350, 60);
+        inP1.setBounds(810, 270, 350, 60);
 
         inP2.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
         inP2.setHorizontalAlignment(javax.swing.JTextField.CENTER);
@@ -168,21 +175,55 @@ public class cPort extends javax.swing.JFrame {
             }
         });
         getContentPane().add(inP2);
-        inP2.setBounds(1250, 290, 350, 60);
+        inP2.setBounds(1220, 270, 350, 60);
+
+        boderInp1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/border.png"))); // NOI18N
+        getContentPane().add(boderInp1);
+        boderInp1.setBounds(370, 260, 1210, 80);
+
+        jLabel9.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
+        jLabel9.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel9.setText(" Port");
+        getContentPane().add(jLabel9);
+        jLabel9.setBounds(280, 380, 460, 60);
+
+        jLabel10.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
+        jLabel10.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel10.setText(" Service");
+        getContentPane().add(jLabel10);
+        jLabel10.setBounds(1210, 380, 460, 60);
+
+        jLabel11.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
+        jLabel11.setForeground(new java.awt.Color(255, 255, 255));
+        jLabel11.setText(" Protocol");
+        getContentPane().add(jLabel11);
+        jLabel11.setBounds(740, 376, 470, 70);
+
+        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/A003.png"))); // NOI18N
+        jLabel6.setToolTipText("");
+        getContentPane().add(jLabel6);
+        jLabel6.setBounds(281, 370, 1389, 80);
 
         jTable1.setFont(new java.awt.Font("Tahoma", 0, 24)); // NOI18N
         jTable1.setModel(model);
-        jTable1.setAutoscrolls(false);
+        jTable1.setColumnSelectionAllowed(true);
+        jTable1.setEnabled(false);
+        jTable1.setFillsViewportHeight(true);
+        jTable1.setGridColor(new java.awt.Color(255, 255, 255));
         jTable1.setRowHeight(30);
-        jTable1.setRowSelectionAllowed(false);
+        jTable1.setSelectionBackground(new java.awt.Color(255, 255, 255));
         jScrollPane1.setViewportView(jTable1);
 
         getContentPane().add(jScrollPane1);
-        jScrollPane1.setBounds(400, 410, 1210, 620);
+        jScrollPane1.setBounds(280, 410, 1390, 570);
 
-        jLabel6.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/border.png"))); // NOI18N
-        getContentPane().add(jLabel6);
-        jLabel6.setBounds(400, 280, 1210, 80);
+        jLabel7.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/border02.png"))); // NOI18N
+        getContentPane().add(jLabel7);
+        jLabel7.setBounds(270, 400, 1410, 590);
+
+        jPanel1.setBackground(new java.awt.Color(255, 255, 255));
+        getContentPane().add(jPanel1);
+        jPanel1.setBounds(280, 420, 1390, 560);
 
         BG.setIcon(new javax.swing.ImageIcon(getClass().getResource("/image/BG.png"))); // NOI18N
         getContentPane().add(BG);
@@ -292,17 +333,23 @@ public class cPort extends javax.swing.JFrame {
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel BG;
     private javax.swing.JButton back;
+    private javax.swing.JLabel boderInp1;
     private javax.swing.JLabel imgBack;
     private javax.swing.JLabel imgTitle;
     private javax.swing.JTextField inP;
     private javax.swing.JTextField inP1;
     private javax.swing.JTextField inP2;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
+    private javax.swing.JLabel jLabel11;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
     private javax.swing.JLabel jLabel6;
+    private javax.swing.JLabel jLabel7;
+    private javax.swing.JLabel jLabel9;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTable1;
     private javax.swing.JLabel title;
